8b91cb300440915fb1b2ee862a6bc21c607a6c50
hark130 <hark130@yahoo.com>
2017-04-15 11:04:54 -0500

    Adjusted to include add_footnote()
    New functionality incorporated.  Signature footnote also hard-coded.
    Comments renumbered to reflect an area that does 'significant work'.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import os.path
 from donjon_module import fix_paths     # fix_paths(htmlCode)
 from donjon_module import remove_hrefs  # remove_hrefs(htmlCode)
+from donjon_module import add_footnote    # add_footnote(htmlCode, newFootnote)
 
 # 1. INPUT VALIDATION
 if sys.argv.__len__() < 2:
@@ -34,27 +35,43 @@ for originalAbsFile in filenames:
     with open(originalAbsFile, 'r') as currentFile:
         originalHTML = currentFile.read()
 
-    ## 3.2. Fix paths
+    ## 3.2. Do the real work 
+
+    ######################    
+    ## 3.2.1. Fix paths ##
+    ######################
     try:
         modifiedHTML = fix_paths(originalHTML)
     except Exception as err:
         print(repr(err)) # DEBUGGING
         raise err
     
-    ## 3.3. Remove any hrefs
+    #############################
+    ## 3.2.2. Remove any hrefs ##
+    #############################
     try:
         modifiedHTML = remove_hrefs(modifiedHTML)
     except Exception as err:
-        print(repr(err))
+        print(repr(err)) # DEBUGGING
         raise err
 
-    ## 3.4. Deconstruct absolute filename
+    #########################
+    ## 3.2.3. Add footnote ##
+    #########################
+    try:
+        newFootnote = '\n<br />\n<br />Results modified by the Donjon_Cleanup project\n<br />Repository:  https://github.com/hark130/Donjon_Cleanup.git'        
+        modifiedHTML = add_footnote(modifiedHTML, newFootnote)
+    except Exception as err:
+        print(repr(err)) # DEBUGGING
+        raise err
+
+    ## 3.3. Deconstruct absolute filename
     originalPath, originalFilename = os.path.split(originalAbsFile)
 
-    ## 3.5. Construct new filename
+    ## 3.4. Construct new filename
     newFilename = originalFilename
 
-    ### 3.5.1. Determine file extension
+    ### 3.4.1. Determine file extension
     if newFilename.find('.') == 0:
         if newFilename.count('.') == 2:
             fileExtension = newFilename[1:]
@@ -67,22 +84,22 @@ for originalAbsFile in filenames:
     else:
         fileExtension = ''
 
-    ### 3.5.2. Strip off file extension
+    ### 3.4.2. Strip off file extension
     if fileExtension.__len__() > 0:
         newFilename = newFilename[:newFilename.find(fileExtension)]
 
-    ### 3.5.3. Create new filename
+    ### 3.4.3. Create new filename
     newFilename = newFilename + '_modified'
 
-    ### 3.5.4. Replace file extension
+    ### 3.4.4. Replace file extension
     newFilename = newFilename + fileExtension
 
-    ## 3.6. Reconstruct absolute filename
+    ## 3.5. Reconstruct absolute filename
     newAbsFile = os.path.join(originalPath, newFilename)
 
-    ## 3.7. Create new file
+    ## 3.6. Create new file
     with open(newAbsFile, 'w') as newFile:
         newFile.write(modifiedHTML)
 
-    ## 3.8. Feedback
+    ## 3.7. Feedback
     print('HTML code found in {} has been successfully modified and saved in {}'.format(originalAbsFile, newAbsFile))
